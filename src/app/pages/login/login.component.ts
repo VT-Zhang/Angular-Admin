9618c85396b3c91b322291c792e7fe2375f9b832
VT-Zhang <jianz@vt.edu>
2017-06-26 14:55:11 -0400

    Corrected some errors

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,35 +9,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.scss'],
   moduleId: module.id,
 })
-export class Login {
+export class Login implements OnInit {
   model: any = {};
   loading = false;
   error = '';
 
-  public form:FormGroup;
-  public email:AbstractControl;
-  public password:AbstractControl;
-  public submitted:boolean = false;
+  // public form:FormGroup;
+  // public email;
+  // public password;
+  // public submitted:boolean = false;
 
-  constructor(fb:FormBuilder,
-              private router: Router,
-              private authenticationService: AuthenticationService) {
-    this.form = fb.group({
-      'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
-    });
+  // constructor(fb:FormBuilder,
+  //             private router: Router,
+  //             private authenticationService: AuthenticationService) {
+  //   this.form = fb.group({
+  //     'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+  //     'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+  //   });
 
-    this.email = this.form.controls['email'];
-    this.password = this.form.controls['password'];
-  }
+  //   this.email = this.form.controls['email'];
+  //   this.password = this.form.controls['password'];
+  // }
 
-  public onSubmit(values:Object):void {
-    this.submitted = true;
-    if (this.form.valid) {
-      // your code goes here
-      // console.log(values);
-    }
-  }
+  constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService) { }
+
+  // public onSubmit(values:Object):void {
+  //   this.loading = true;
+  //   if (this.form.valid) {
+  //       this.authenticationService.login(this.email, this.password)
+  //           .subscribe(result => {
+  //               if (result === true) {
+  //                   this.router.navigate(['/']);
+  //               } else {
+  //                   this.error = 'Username or password is incorrect';
+  //                   this.loading = false;
+  //               }
+  //           });
+  //   }
+  // }
 
 
   ngOnInit() {
@@ -45,9 +56,9 @@ export class Login {
         this.authenticationService.logout();
     }
 
-  login() {
+   login() {
         this.loading = true;
-        this.authenticationService.login(this.model.email, this.model.password)
+        this.authenticationService.login(this.model.username, this.model.password)
             .subscribe(result => {
                 if (result === true) {
                     this.router.navigate(['/']);
